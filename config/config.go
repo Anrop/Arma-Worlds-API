9ee97fe507985cdccb5fba7f2504e6de2d6d40ad
Björn Dahlgren <bjorn@dahlgren.at>
2019-08-12 09:25:40 +0200

    Add satellite map tiles

--- config/config.go
+++ config/config.go
@@ -7,6 +7,7 @@ import (
 type Config struct {
 	DatabaseURL             string
 	Port                    string
+	SatelliteTilesBaseURL   string
 	TopographicTilesBaseURL string
 }
 
@@ -14,6 +15,7 @@ func FromEnv() (config Config) {
 	config = Config{
 		DatabaseURL:             os.Getenv("DATABASE_URL"),
 		Port:                    os.Getenv("PORT"),
+		SatelliteTilesBaseURL:   os.Getenv("SATELLITE_TILES_BASE_URL"),
 		TopographicTilesBaseURL: os.Getenv("TOPOGRAPHIC_TILES_BASE_URL"),
 	}
 
