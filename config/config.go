b1fb6996ee42c87f7bdd50cdd40e1f418dab78cc
Björn Dahlgren <bjorn@dahlgren.io>
2022-01-25 23:42:31 +0100

    Add URL to world locations

--- config/config.go
+++ config/config.go
@@ -7,6 +7,7 @@ import (
 // Config model
 type Config struct {
 	DatabaseURL         string
+	LocationsURL        string
 	Port                string
 	SatelliteTilesURL   string
 	TopographicTilesURL string
@@ -16,6 +17,7 @@ type Config struct {
 func FromEnv() (config Config) {
 	config = Config{
 		DatabaseURL:         os.Getenv("DATABASE_URL"),
+		LocationsURL:        os.Getenv("LOCATIONS_URL"),
 		Port:                os.Getenv("PORT"),
 		SatelliteTilesURL:   os.Getenv("SATELLITE_TILES_URL"),
 		TopographicTilesURL: os.Getenv("TOPOGRAPHIC_TILES_URL"),
